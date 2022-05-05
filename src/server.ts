56aa11247e369356936c1947954865e1fd8a6df0
Exlley Santos <exlleyclemente@gmail.com>
2022-05-04 21:18:14 -0300

    feat: add mail sender

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,36 @@
 import express, { Request, Response } from "express";
+import nodemailer from "nodemailer";
 
 import prisma from "./prisma";
 
 const server = express();
 
 server.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "653bcb79f97863",
+    pass: "9e2a342b7b981b",
+  },
+});
+
 server.post("/feedbacks", async (req: Request, res: Response) => {
   const { type, comment, screenshot } = req.body;
 
   const feedback = await prisma.feedback.create({
     data: { type, comment, screenshot },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <feedget@feedget.com>",
+    to: "Exlley santos <exlleyclemente@gmail.com>",
+    subject: "New Feedback",
+    html: [
+      `<p>Seu Feedback ${type} foi enviado<p>`,
+      `<p>comentário : ${comment}`,
+    ].join("\n"),
   });
   res.status(202).json({ data: feedback });
 });
