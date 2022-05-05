72b78357fec4efdd2f4d60ceb5c5dc7b83c10039
Exlley Santos <exlleyclemente@gmail.com>
2022-05-04 21:06:18 -0300

    feat: creat feedback route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express";
 
+import prisma from "./prisma";
+
 const server = express();
 
 server.use(express.json());
-server.get("/", (req: Request, res: Response) => {
-  res.send({ message: "Hi" });
+server.post("/feedbacks", async (req: Request, res: Response) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: { type, comment, screenshot },
+  });
+  res.status(202).json({ data: feedback });
 });
 
 const PORT = 3333;
